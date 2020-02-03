4c0f5719e537e8860091cb0cfa2486ec436c2bb3
Ivo Angelov <jc@jagdcake.com>
2020-02-03 18:43:32 +0200

    Remove unnecessary CSS imports
    Both pages use the layout component which imports the CSS file.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import "../components/main.css"
 
 const IndexPage: FunctionComponent = (): ReactElement => (
   <Layout>
--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -5,7 +5,6 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import "../components/main.css"
 
 const SecondPage: FunctionComponent = (): ReactElement => (
   <Layout>
