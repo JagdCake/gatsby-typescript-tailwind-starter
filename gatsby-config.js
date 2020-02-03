1d189efec00caf1a9968f8e7f1fc12143745b486
Ivo Angelov <jc@jagdcake.com>
2020-02-03 16:12:28 +0200

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ require("ts-node").register({ files: true });
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby TypeScript Tailwind Starter`,
+    description: `Kick off your next, great Gatsby project with this starter. This barebones starter ships with the main Gatsby configuration files you might need, TypeScript and Tailwind CSS support.`,
+    author: `JagdCake`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
