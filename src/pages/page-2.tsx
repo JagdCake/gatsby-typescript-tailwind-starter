4cc409c6bdbeb74669dd53ad89a00d31b0142180
Ivo Angelov <jc@jagdcake.com>
2020-02-03 14:25:38 +0200

    Update second page to use TypeScript

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,10 +1,12 @@
 import React from "react"
+import { FunctionComponent } from "react"
+import { ReactElement } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage: FunctionComponent = (): ReactElement => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
