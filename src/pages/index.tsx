74148893332f494ea2835e83e593c9901f5f10b9
Ivo Angelov <jc@jagdcake.com>
2020-02-03 17:41:03 +0200

    Replace inline styles with Tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "../components/main.css"
 
 const IndexPage: FunctionComponent = (): ReactElement => (
   <Layout>
@@ -13,7 +14,7 @@ const IndexPage: FunctionComponent = (): ReactElement => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div className="max-w-xs mb-6" >
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
