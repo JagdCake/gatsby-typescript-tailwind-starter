a0d67e55fedf05e6516bdfb6d4ca28c91f428488
Ivo Angelov <jc@jagdcake.com>
2020-04-24 11:26:13 +0300

    Test if header contains link to home page

--- src/components/header.test.tsx
+++ src/components/header.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import Header from './header';
 
 describe('Header', () => {
@@ -7,5 +8,11 @@ describe('Header', () => {
         const tree = renderer.create(<Header siteTitle="Starter" />).toJSON();
 
         expect(tree).toMatchSnapshot();
+    });
+
+    it('contains a link to the home page', () => {
+        const { getByText } = render(<Header siteTitle="Starter" />);
+
+        expect(getByText('Starter')).toHaveAttribute('href', '/');
     });
 });
